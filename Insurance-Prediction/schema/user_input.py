a63d1f8233d11e7872d9c87377ab1308f44e01cd
VaibhavSaxena10 <saxenavaibhav161@gmail.com>
2026-08-17 22:27:17 +0530

    Bug fixed

--- Insurance-Prediction/schema/user_input.py
+++ Insurance-Prediction/schema/user_input.py
@@ -25,7 +25,7 @@ class UserInput(BaseModel):
 
     @computed_field
     @property
-    def lifestyle(self)-> str:
+    def lifestyle_risk(self)-> str:
         if self.smoker and self.bmi > 30:
             return "high"
         elif self.smoker or self.bmi > 27:
